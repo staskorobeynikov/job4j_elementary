721a19176f8f8670c38da2e8b7188896b4a4c700
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-03-12 21:52:03 +0300

    4. Оператор ветвления[#181]

---
+++ src/ru/job4j/calculator/MathFunc.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculator;
+
+public class MathFunc {
+    public static int func1(int x) {
+        return x * x + 1;
+    }
+
+    public static int func2(int x) {
+        return 1 / x;
+    }
+
+    public static void main(String[] args) {
+        double result1 = MathFunc.func2(3);
+        double result2 = MathFunc.func2(5);
+        double total = result1 + result2;
+        System.out.println(total);
+    }
+}
