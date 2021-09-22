1e06aa0c5a8d1f9e66e4065945b714d4e0ec5f09
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-09-22 21:24:31 +0300

    usage Method Reference

---
+++ src/main/java/ru/job4j/lambda/MRFunction.java
@@ -0,0 +1,10 @@
+package ru.job4j.lambda;
+
+import java.util.function.Function;
+
+public class MRFunction {
+
+    public static Function<Double, Double> apply() {
+        return Math::sqrt;
+    }
+}
---
+++ src/test/java/ru/job4j/lambda/MRFunctionTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.*;
+
+public class MRFunctionTest {
+
+    @Test
+    public void whenApply4ThenEquals2() {
+        Function<Double, Double> f = MRFunction.apply();
+        assertEquals(2.0, f.apply(4.0), 0.01);
+    }
+
+    @Test
+    public void whenApply9ThenEquals3() {
+        Function<Double, Double> f = MRFunction.apply();
+        assertEquals(3.0, f.apply(9.0), 0.01);
+    }
+}
