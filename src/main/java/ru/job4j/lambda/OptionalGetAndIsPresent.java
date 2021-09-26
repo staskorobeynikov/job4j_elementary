81082a0955891eae4a03483176d33f18942542e6
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-09-26 20:18:17 +0300

    usage methods class Optional

---
+++ src/main/java/ru/job4j/lambda/OptionalGetAndIsPresent.java
@@ -0,0 +1,20 @@
+package ru.job4j.lambda;
+
+import java.util.Optional;
+
+public class OptionalGetAndIsPresent {
+
+    public static int get(int[] data, int el) {
+        Optional<Integer> result = indexOf(data, el);
+        return result.isPresent() ? result.get() : -1;
+    }
+
+    private static Optional<Integer> indexOf(int[] data, int el) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                return Optional.of(i);
+            }
+        }
+        return Optional.empty();
+    }
+}
