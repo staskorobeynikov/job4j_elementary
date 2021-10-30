d197a3f717251e3faa9197a29c942be716e63760
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-10-30 21:27:26 +0300

    terminal methods - forEach and allMatch

--- src/test/java/ru/job4j/stream/StreamIterateTest.java
+++ src/test/java/ru/job4j/stream/StreamIterateTest.java
@@ -18,8 +18,8 @@ public class StreamIterateTest {
         StreamIterate.showArray(data);
         String ln = System.lineSeparator();
         assertThat(out.toString(), is(
-                "1" + ln +
-                        "3" + ln
+                "1" + ln
+                        + "3" + ln
         ));
     }
 }
