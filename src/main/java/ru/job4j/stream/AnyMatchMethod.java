000ff457dcc89a8ebcaa193c57a5270db29fb354
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-11-01 21:16:44 +0300

    terminal methods - anyMatch and noneMatch

---
+++ src/main/java/ru/job4j/stream/AnyMatchMethod.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+public class AnyMatchMethod {
+
+    public static boolean check(List<String> data) {
+        return data
+                .stream()
+                .anyMatch(s -> s.startsWith("job4j"));
+    }
+}
