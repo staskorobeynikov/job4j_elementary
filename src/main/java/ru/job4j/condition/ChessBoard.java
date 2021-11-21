5f6451e762c7b6e2f837d24a926aa4a2fa57282a
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-11-21 21:50:48 +0300

    update with uses var-args

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if (validate(x1) && validate(x2) && validate(y1) && validate(y2)) {
+        if (check(x1, x2, y1, y2)) {
             if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 rsl = Math.abs(x2 - x1);
             }
@@ -13,5 +13,14 @@ public class ChessBoard {
 
     private static boolean validate(int coordinate) {
         return coordinate >= 0 && coordinate <= 7;
+    }
+
+    private static boolean check(int... coordinates) {
+        for (int c : coordinates) {
+            if (c < 0 || c > 7) {
+                return false;
+            }
+        }
+        return true;
     }
 }
