73bc3439592253249e55cffeff751fcf3111f2a1
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-11-23 21:42:22 +0300

    intermediate method flatMap() fot primitive and objects

---
+++ src/main/java/ru/job4j/stream/FlatMapForPrimitive.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+
+public class FlatMapForPrimitive {
+    public static int sum(int[][] matrix) {
+        return Arrays
+                .stream(matrix)
+                .flatMapToInt(Arrays::stream)
+                .sum();
+    }
+}
