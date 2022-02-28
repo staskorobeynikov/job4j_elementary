4d6e40387e3c9e6ea4ced226efcc3316c20d1a64
staskorobeynikov <stas.korobeinikov@mail.ru>
2022-02-28 20:24:43 +0300

    optimization method check prime number

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,12 +2,11 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number > 1;
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
         }
-        return prime;
+        return number > 1;
     }
 }
