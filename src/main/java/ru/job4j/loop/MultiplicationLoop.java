0d6a89da256c2b8a8c76b1ca1fbd47a941be2658
staskorobeynikov <stas.korobeinikov@mail.ru>
2022-06-06 19:24:53 +0300

    0.7. Найти степень числа 0.6. Произведение чисел в диапазоне

---
+++ src/main/java/ru/job4j/loop/MultiplicationLoop.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class MultiplicationLoop {
+    public static int mult(int a, int b) {
+        int rsl = 1;
+        for (int i = a; i <= b; i++) {
+            rsl *= i;
+        }
+        return rsl;
+    }
+}
