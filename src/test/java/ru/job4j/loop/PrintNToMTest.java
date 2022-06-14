2ebc02af68185ed6258f7f39e86760f414191b4e
staskorobeynikov <stas.korobeinikov@mail.ru>
2022-06-14 19:39:04 +0300

    0.1. Вывести на консоль числа от 0 до N 0.2. Вывести на консоль числа от N до M 0.3. Вывести на консоль числа от N до 0 0.4. Вывести на консоль числа от M до N 0.5. Вывести стоимость конфет за килограмм

---
+++ src/test/java/ru/job4j/loop/PrintNToMTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PrintNToMTest {
+    @Test
+    public void out2to4() {
+        var out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        PrintNToM.out(2, 4);
+        var expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("2").add("3").toString();
+        assertThat(out.toString(), is(expect));
+    }
+
+    @Test
+    public void out1to3() {
+        var out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        PrintNToM.out(1, 3);
+        var expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("1").add("2").toString();
+        assertThat(out.toString(), is(expect));
+    }
+}
---
+++ src/test/java/ru/job4j/loop/SweetPriceTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SweetPriceTest {
+    @Test
+    public void test() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        SweetPrice.out(10);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "10" + ln +
+                        "20" + ln +
+                        "30" + ln +
+                        "40" + ln +
+                        "50" + ln
+        ));
+    }
+}
