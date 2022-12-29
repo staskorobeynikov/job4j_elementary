bcef20b5d238abec9925d578779cfa052544476a
staskorobeynikov <stas.korobeinikov@mail.ru>
2022-12-29 19:39:52 +0300

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,39 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder builder = new StringBuilder();
+        int size = evenElements.size();
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) {
+                builder.append(evenElements.pollFirst());
+            } else {
+                evenElements.pollFirst();
+            }
+        }
+        return builder.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder builder = new StringBuilder();
+        while (!descendingElements.isEmpty()) {
+            builder.append(descendingElements.pollLast());
+        }
+        return builder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return  getEvenElements() + getDescendingElements();
+    }
+}
