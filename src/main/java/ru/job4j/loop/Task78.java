3f4f06e6c3d75473f922cd5b9a9c889fa82e253f
staskorobeynikov <stas.korobeinikov@mail.ru>
2023-08-13 20:49:47 +0300

    152. Вывести двузначные числа согласно условию

--- src/main/java/ru/job4j/loop/Task78.java
+++ src/main/java/ru/job4j/loop/Task78.java
@@ -1,7 +1,7 @@
 package ru.job4j.loop;
 
 public class Task78 {
-    public static void loop(int[] days, int[] values ) {
+    public static void loop(int[] days, int[] values) {
         int x = 0;
         int y = 0;
         for (int i = 0; i < days.length; i++) {
