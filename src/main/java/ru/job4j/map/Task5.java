a7acfb6d576f75ebdf1aba65d9b15691f3d99871
staskorobejnikov <stas.korobeinikov@mail.ru>
2023-11-27 11:48:30 +0400

    5. Супер работник

---
+++ src/main/java/ru/job4j/map/Task5.java
@@ -0,0 +1,27 @@
+package ru.job4j.map;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task5 {
+    public static List<Integer> multiAssign(List<Task> tasks) {
+        Map<Integer, List<Task>> buffer = new LinkedHashMap<>();
+        for (Task task : tasks) {
+            List<Task> taskList = buffer.getOrDefault(task.assignId, new ArrayList<>());
+            taskList.add(task);
+            buffer.put(task.assignId(), taskList);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (Map.Entry<Integer, List<Task>> entry : buffer.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                result.add(entry.getKey());
+            }
+        }
+        return result;
+    }
+
+    public record Task(Integer taskId, Integer assignId) {
+    }
+}
---
+++ src/test/java/ru/job4j/map/Task5Test.java
@@ -0,0 +1,33 @@
+package ru.job4j.map;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task5Test {
+    @Test
+    public void whenMulti() {
+        var input = List.of(
+                new Task5.Task(1, 1),
+                new Task5.Task(1, 2),
+                new Task5.Task(1, 1)
+        );
+        var expected = List.of(1);
+        var result = Task5.multiAssign(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenOnlyUnique() {
+        var input = List.of(
+                new Task5.Task(1, 1),
+                new Task5.Task(1, 2),
+                new Task5.Task(1, 3)
+        );
+        var expected = List.of();
+        var result = Task5.multiAssign(input);
+        assertThat(result).isEqualTo(expected);
+    }
+}
