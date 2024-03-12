b0adf3f33a06854c1271874a93cd7764f10ca657
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-03-12 20:37:01 +0400

    Анализ лога HTTP сервера второй метод

--- src/main/java/ru/job4j/map/AnalyzeHttpLog.java
+++ src/main/java/ru/job4j/map/AnalyzeHttpLog.java
@@ -1,8 +1,10 @@
 package ru.job4j.map;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class AnalyzeHttpLog {
@@ -17,5 +19,24 @@ public class AnalyzeHttpLog {
                         LinkedHashMap::new,
                         Collectors.counting()
                 ));
+    }
+
+    public static String maxByThread(List<Line> logs) {
+        return logs.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Line::thread,
+                                Collectors.summingInt(line -> line.text.getBytes().length)
+                        )
+                ).entrySet()
+                .stream()
+                .max(Comparator.comparingInt(Entry::getValue))
+                .map(Entry::getKey)
+                .orElse(null);
+    }
+
+    public static Map<String, Long> detailByThread(List<Line> logs, String thread,
+                                                   List<String> orders) {
+        return Map.of();
     }
 }
--- src/test/java/ru/job4j/map/AnalyzeHttpLogTest.java
+++ src/test/java/ru/job4j/map/AnalyzeHttpLogTest.java
@@ -18,4 +18,15 @@ class AnalyzeHttpLogTest {
         );
         assertThat(AnalyzeHttpLog.groupByLevel(logs)).isEqualTo(Map.of("ERROR", 3L, "DEBUG", 1L));
     }
+
+    @Test
+    public void whenMaxByThread() {
+        var logs = List.of(
+                new AnalyzeHttpLog.Line("ERROR", "[threads [qbxb [bbb]] - 1]", "yaee ybze"),
+                new AnalyzeHttpLog.Line("ERROR", "[threads [xcde [aqx]] - 0]", "wyce"),
+                new AnalyzeHttpLog.Line("ERROR", "[threads [wcxf [czed [ayya [fye]]]] - 0]", "eaba yzd"),
+                new AnalyzeHttpLog.Line("DEBUG", "[threads [zqfz [wbye [zfq]]] - 1]", "[cz] bxa")
+        );
+        assertThat(AnalyzeHttpLog.maxByThread(logs)).isEqualTo("[threads [qbxb [bbb]] - 1]");
+    }
 }
