48f155071906dc597b814799f8b44f30efc07b2e
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-05-27 19:21:13 +0400

    20. Вывод значений двух переменных с поясняющими надписями

---
+++ src/main/java/ru/job4j/io/Task20.java
@@ -0,0 +1,11 @@
+package ru.job4j.io;
+
+public class Task20 {
+    public static void print(int x, int y) {
+        int sum = x + y;
+        int multiple = x * y;
+        System.out.printf("Переменные: %s %s%n", x, y);
+        System.out.printf("Сумма: %s%n", sum);
+        System.out.printf("Произведение: %s%n", multiple);
+    }
+}
