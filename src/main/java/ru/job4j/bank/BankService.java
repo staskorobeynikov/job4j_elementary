9ec1db731c388cf68ce865dfe0a004bab4bb55b3
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-06-26 20:16:11 +0400

    3. Банковские переводы [#10038]. First commit

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,41 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.put(user, new ArrayList<>());
+    }
+
+    public void deleteUser(String passport) {
+        users.remove(new User(passport, ""));
+    }
+
+    public void addAccount(String passport, Account account) {
+
+    }
+
+    public User findByPassport(String passport) {
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String sourcePassport, String sourceRequisite,
+                                 String destinationPassport, String destinationRequisite,
+                                 double amount) {
+        boolean result = false;
+        return result;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
