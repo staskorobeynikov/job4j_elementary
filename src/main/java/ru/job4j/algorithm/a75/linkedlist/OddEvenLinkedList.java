8a53734695c295c0e77ea268ff49f78968d7f5de
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-12-25 21:07:12 +0800

    Odd Even Linked List

---
+++ src/main/java/ru/job4j/algorithm/a75/linkedlist/OddEvenLinkedList.java
@@ -0,0 +1,37 @@
+package ru.job4j.algorithm.a75.linkedlist;
+
+public class OddEvenLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
---
+++ src/test/java/ru/job4j/algorithm/a75/linkedlist/OddEvenLinkedListTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.algorithm.a75.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class OddEvenLinkedListTest {
+    @Test
+    void testOddLengthList() {
+        OddEvenLinkedList listHelper = new OddEvenLinkedList();
+        OddEvenLinkedList.ListNode head = createList(new int[]{1, 2, 3, 4, 5}, listHelper);
+        OddEvenLinkedList.ListNode result = listHelper.oddEvenList(head);
+        assertArrayEquals(new int[]{1, 3, 5, 2, 4}, toArray(result), "Expected order is [1, 3, 5, 2, 4]");
+    }
+
+    @Test
+    void testEvenLengthList() {
+        OddEvenLinkedList listHelper = new OddEvenLinkedList();
+        OddEvenLinkedList.ListNode head = createList(new int[]{1, 2, 3, 4, 5, 6}, listHelper);
+        OddEvenLinkedList.ListNode result = listHelper.oddEvenList(head);
+        assertArrayEquals(new int[]{1, 3, 5, 2, 4, 6}, toArray(result), "Expected order is [1, 3, 5, 2, 4, 6]");
+    }
+
+    @Test
+    void testSingleNodeList() {
+        OddEvenLinkedList listHelper = new OddEvenLinkedList();
+        OddEvenLinkedList.ListNode head = createList(new int[]{1}, listHelper);
+        OddEvenLinkedList.ListNode result = listHelper.oddEvenList(head);
+        assertArrayEquals(new int[]{1}, toArray(result), "Single-node list remains unchanged");
+    }
+
+    @Test
+    void testTwoNodeList() {
+        OddEvenLinkedList listHelper = new OddEvenLinkedList();
+        OddEvenLinkedList.ListNode head = createList(new int[]{1, 2}, listHelper);
+        OddEvenLinkedList.ListNode result = listHelper.oddEvenList(head);
+        assertArrayEquals(new int[]{1, 2}, toArray(result), "Two-node list remains unchanged");
+    }
+
+    @Test
+    void testEmptyList() {
+        OddEvenLinkedList listHelper = new OddEvenLinkedList();
+        OddEvenLinkedList.ListNode result = listHelper.oddEvenList(null);
+        assertNull(result, "Empty list should return null");
+    }
+
+    @Test
+    void testAllEvenValues() {
+        OddEvenLinkedList listHelper = new OddEvenLinkedList();
+        OddEvenLinkedList.ListNode head = createList(new int[]{2, 4, 6, 8}, listHelper);
+        OddEvenLinkedList.ListNode result = listHelper.oddEvenList(head);
+        assertArrayEquals(new int[]{2, 6, 4, 8}, toArray(result), "Odd-indexed and even-indexed values should rearrange correctly");
+    }
+
+    private OddEvenLinkedList.ListNode createList(int[] values, OddEvenLinkedList helper) {
+        OddEvenLinkedList.ListNode dummy = helper.new ListNode();
+        OddEvenLinkedList.ListNode current = dummy;
+        for (int value : values) {
+            current.next = helper.new ListNode(value);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    private int[] toArray(OddEvenLinkedList.ListNode head) {
+        if (head == null) return new int[0];
+        java.util.List<Integer> list = new java.util.ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
