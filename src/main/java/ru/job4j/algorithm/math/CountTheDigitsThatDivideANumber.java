12243940d7ddd6be992d42ee4832565393df50a5
staskorobejnikov <stas.korobeinikov@mail.ru>
2025-02-11 14:08:21 +0700

    Count the Digits That Divide a Number

--- src/main/java/ru/job4j/algorithm/math/CountTheDigitsThatDivideANumber.java
+++ src/main/java/ru/job4j/algorithm/math/CountTheDigitsThatDivideANumber.java
@@ -1,4 +1,14 @@
 package ru.job4j.algorithm.math;
 
 public class CountTheDigitsThatDivideANumber {
+    public int countDigits(int num) {
+        int count = 0;
+        for (int current = num; current > 0; current /= 10) {
+            int digit = current % 10;
+            if (digit != 0 && num % digit == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/ru/job4j/algorithm/math/CountTheDigitsThatDivideANumberTest.java
+++ src/test/java/ru/job4j/algorithm/math/CountTheDigitsThatDivideANumberTest.java
@@ -1,4 +1,41 @@
+package ru.job4j.algorithm.math;
+
 import static org.junit.jupiter.api.Assertions.*;
-class CountTheDigitsThatDivideANumberTest {
-  
+import org.junit.jupiter.api.Test;
+
+public class CountTheDigitsThatDivideANumberTest {
+
+    @Test
+    void testSingleDigitNumber() {
+        CountTheDigitsThatDivideANumber solution = new CountTheDigitsThatDivideANumber();
+        assertEquals(1, solution.countDigits(7), "7 is divisible by 7 itself");
+        assertEquals(1, solution.countDigits(3), "3 is divisible by 3 itself");
+    }
+
+    @Test
+    void testMultipleDivisibleDigits() {
+        CountTheDigitsThatDivideANumber solution = new CountTheDigitsThatDivideANumber();
+        assertEquals(2, solution.countDigits(12), "Digits 1 and 2 divide 12");
+        assertEquals(3, solution.countDigits(128), "Digits 1, 2, and 8 divide 128");
+    }
+
+    @Test
+    void testContainsZero() {
+        CountTheDigitsThatDivideANumber solution = new CountTheDigitsThatDivideANumber();
+        assertEquals(2, solution.countDigits(101), "Only digits 1 and 1 divide 101");
+        assertEquals(1, solution.countDigits(10), "Only digit 1 divides 10 (0 is ignored)");
+    }
+
+    @Test
+    void testNumberWithNoDivisibleDigits() {
+        CountTheDigitsThatDivideANumber solution = new CountTheDigitsThatDivideANumber();
+        assertEquals(0, solution.countDigits(23), "Neither 2 nor 3 divides 23");
+    }
+
+    @Test
+    void testEdgeCases() {
+        CountTheDigitsThatDivideANumber solution = new CountTheDigitsThatDivideANumber();
+        assertEquals(0, solution.countDigits(0), "0 should return 0");
+        assertEquals(1, solution.countDigits(1), "1 is divisible by itself");
+    }
 }
